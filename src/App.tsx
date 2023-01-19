bc508e81b5ba19b79c05d5dbe26edec75050238b
Miaritiana <hei.miaritiana@gmail.com>
2023-01-19 21:26:56 +0300

    chore: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img className='App-logo' src="logo.png" alt="LOGO" />
+        </div>
+        <div className='btn-login'>
+          <div className='App-header-btn'>S'inscrire</div>
+          <div className='App-header-btn'>Se connecter</div>
+        </div>
       </header>
+      <div className='main'>
+        <div className='main-card'>
+          <h4>Nos services</h4>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis cum eum saepe aut quo nisi autem consequatur odit, distinctio suscipit deserunt inventore veritatis laboriosam facilis pariatur voluptatibus quod veniam atque!
+            <br/>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <div className='main-card'>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis cum eum saetempora asperiores repellendus, pe aut quo nisi autem consequatur odit, distinctio suscipit deserunt inventore veritatis laboriosam facilis pariatur voluptatibus quod veniam atque!
+            <br/>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <div className='App-header-btn'>Commencez</div>
+        </div>
+      </div>
     </div>
   );
 }
